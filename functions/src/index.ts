10b46f1d1cff29d424cc77a7f3016bd52416239b
tkawabat <tosiki.kawabata@gmail.com>
2019-08-16 15:06:34 +0900

    initail

---
+++ functions/src/index.ts
@@ -0,0 +1,33 @@
+// import * as functions from 'firebase-functions';
+
+// // // Start writing Firebase Functions
+// // // https://firebase.google.com/docs/functions/typescript
+// //
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//     response.send("Hello from Firebase!");
+// });
+
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
+
+admin.initializeApp(functions.config().firebase);
+
+export const createAccountDoc = functions.auth.user().onCreate( async (user) => {
+  const db = admin.firestore();
+  const batch = db.batch();
+
+  const userCollection = db.collection('User');
+  const userRef = userCollection.doc(user.uid);
+
+  try{
+    await batch.set(userRef, { name: '未設定' });
+
+    await batch.commit().then(() => {
+    console.log('add user success.');
+    })
+  }
+  catch(e) {
+    console.log(`error occurs: ${e}`);
+  }
+});
+
